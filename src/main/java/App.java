02b7518f4270b540177c8c4754a7a7fec3aab0fb
Georgina Van Dort and Stephen Emery <georgina@>
2016-09-29 14:22:44 -0700

    add a bunch of front end

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,7 +18,24 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/blog/:blog_id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      int blog_id = Integer.parseInt(request.params(":blog_id"));
+      Blog blog = Blog.find(blog_id);
+      model.put("tags", blog.getTagsByBlog());
+      model.put("blogs", blog);
+      model.put("template", "templates/blog.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
+    get("/tag/:tag_id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      int tag_id = Integer.parseInt(request.params(":tag_id"));
+      Tag tag = Tag.find(tag_id);
+      model.put("tags", tag);
+      model.put("template", "templates/tag.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
   }
 }
